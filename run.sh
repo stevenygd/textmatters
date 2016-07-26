e12c3c3d18f5e8ef3cade4b287caf47ea3686dcf
Yifan Zhu <yz542@cornell.edu>
2016-07-26 16:51:43 +0000

    Merging

--- run.sh
+++ run.sh
@@ -1,4 +1,4 @@
 #! /bin/bash
 
 #run script 
-CUDA_VISIBLE_DEVICES=3 python pipeline.py --input_file no_texts_img_ids.pkl --out_file no_text_black_bg  --ablt_meth destroy  --batch_size 64 
+CUDA_VISIBLE_DEVICES=0 python pipeline.py --input_file toilet_img_ids --out_file toilet_only_median_bg  --ablt_meth median_bg  --batch_size 32 --category toilet 
